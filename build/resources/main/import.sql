b469b3316f325136b19d0543ac73eec04fa07df7
antonio-maldonado <67517910+antonio-maldonado@users.noreply.github.com>
2024-05-08 13:34:00 -0600

    feat: project build and deploy

---
+++ build/resources/main/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users( email, password ) VALUES ( 'antonio@gmail.com', '$2a$10$9ERRfNmh9XvGji77p6d57OLWx3wSjoFO8qlYFttcMCZRkxchUDiPe');
+INSERT INTO users( email, password ) VALUES ( 'gerardo@iwa.com', '$2a$12$EyNFwC27HjUFwzmQN46syuMTpvFCIZuBvs3jZ6UAM4TRGGyRnBTsW');
